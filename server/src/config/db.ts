dc28ff59dbe4396ea008342a17f90c728af7ef5d
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-10-15 14:38:18 -0300

    feat: create database connection

---
+++ server/src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || '');
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Error: ${error.message}`);
+    } else {
+      console.error('Unknown error occurred');
+    }
+    process.exit(1); 
+  }
+};
+
+export default connectDB;
